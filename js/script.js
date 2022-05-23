97b2896721961078d48fe0befc9c2c691c80e5e4
Cindy <cindy.nsc1@gmail.com>
2022-05-23 12:54:35 -0300

    refactor animais-fetch

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -29,6 +29,7 @@ console.log(tooltip);
 initAnimacaoScroll();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
 initDropdownMenu();
+
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
